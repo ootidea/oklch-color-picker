ab96e06e082ed70c2d24ba0d003bcf13799493ff
ootidea <ootidea@gmail.com>
2023-04-14 09:36:04 +0900

    refactor: split file about color calculation

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,7 @@
 import { clamp, isInstanceOf, modOf, rangeUntil, roundAt } from 'base-up'
-import Color from 'colorjs.io'
 import { createMemo, createSignal } from 'solid-js'
 import classes from './App.module.scss'
-
-/** calculateMaxChromaInGamut関数の戻り値をメモ化するためのオブジェクト */
-const maxChromaMemo: Record<string, number> = {}
-
-/** sRGBの色域に収まる最大のchroma値を二分法で探索する */
-function calculateMaxChromaInGamut(lightness: number, hue: number, delta: number = 0.001): number {
-  const memoKey = `${lightness},${hue}`
-  const cachedValue = maxChromaMemo[memoKey]
-  if (cachedValue !== undefined) return cachedValue
-
-  // 二分法の探索範囲の下限
-  let lowerBound = 0
-  // 二分法の探索範囲の上限。
-  // sRGBでの最大値は0.321台であり、0.322以下だと分かっている。
-  // そのことは次のウェブページでも確認できる。
-  // https://oklch.evilmartians.io/#69.9,0.321,328.24,100
-  let upperBound = 0.322
-  while (upperBound - lowerBound > delta) {
-    const chroma = lowerBound + (upperBound - lowerBound) / 2
-    if (new Color('oklch', [lightness, chroma, hue], 1).inGamut('srgb')) {
-      lowerBound = chroma
-    } else {
-      upperBound = chroma
-    }
-  }
-  return (maxChromaMemo[memoKey] = lowerBound)
-}
-
-function createColorByChromaRatio(lightness: number, chromaRatio: number, hue: number) {
-  const maxChroma = calculateMaxChromaInGamut(lightness, hue)
-  return new Color('oklch', [lightness, maxChroma * chromaRatio, hue], 1)
-}
-
-function toHsl(lightness: number, chromaRatio: number, hue: number): string {
-  return createColorByChromaRatio(lightness, chromaRatio, hue).to('hsl').toString()
-}
+import { createColorByChromaRatio, toHsl } from './color'
 
 function createHueSignal() {
   const [hue, setHue] = createSignal(120)
---
+++ src/color.ts
@@ -0,0 +1,37 @@
+import Color from 'colorjs.io'
+
+/** calculateMaxChromaInGamut関数の戻り値をメモ化するためのオブジェクト */
+const maxChromaMemo: Record<string, number> = {}
+
+/** sRGBの色域に収まる最大のchroma値を二分法で探索する */
+function calculateMaxChromaInGamut(lightness: number, hue: number, delta: number = 0.001): number {
+  const memoKey = `${lightness},${hue}`
+  const cachedValue = maxChromaMemo[memoKey]
+  if (cachedValue !== undefined) return cachedValue
+
+  // 二分法の探索範囲の下限
+  let lowerBound = 0
+  // 二分法の探索範囲の上限。
+  // sRGBでの最大値は0.321台であり、0.322以下だと分かっている。
+  // そのことは次のウェブページでも確認できる。
+  // https://oklch.evilmartians.io/#69.9,0.321,328.24,100
+  let upperBound = 0.322
+  while (upperBound - lowerBound > delta) {
+    const chroma = lowerBound + (upperBound - lowerBound) / 2
+    if (new Color('oklch', [lightness, chroma, hue], 1).inGamut('srgb')) {
+      lowerBound = chroma
+    } else {
+      upperBound = chroma
+    }
+  }
+  return (maxChromaMemo[memoKey] = lowerBound)
+}
+
+export function createColorByChromaRatio(lightness: number, chromaRatio: number, hue: number) {
+  const maxChroma = calculateMaxChromaInGamut(lightness, hue)
+  return new Color('oklch', [lightness, maxChroma * chromaRatio, hue], 1)
+}
+
+export function toHsl(lightness: number, chromaRatio: number, hue: number): string {
+  return createColorByChromaRatio(lightness, chromaRatio, hue).to('hsl').toString()
+}
