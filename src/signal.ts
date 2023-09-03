8f26ee25298516d4ab09c4fa45d3cc93d5155e16
ootidea <ootidea@gmail.com>
2023-09-03 13:37:13 +0900

    fix: hue NaN inf loop bug

--- src/signal.ts
+++ src/signal.ts
@@ -7,15 +7,36 @@ export const [lightness, setLightness] = createLightnessSignal()
 
 function createHueSignal() {
   const [hue, setHue] = createSignal(180)
-  return [hue, (newHue: number) => setHue(clamp(0, newHue, 360))] as const
+  return [
+    hue,
+    (newHue: number) => {
+      if (Number.isNaN(newHue)) return
+
+      setHue(clamp(0, newHue, 360))
+    },
+  ] as const
 }
 
 function createChromaRatioSignal() {
   const [chromaRatio, setChromaRatio] = createSignal(0.8)
-  return [chromaRatio, (newChromaRatio: number) => setChromaRatio(clamp(0, newChromaRatio, 1))] as const
+  return [
+    chromaRatio,
+    (newChromaRatio: number) => {
+      if (Number.isNaN(newChromaRatio)) return
+
+      setChromaRatio(clamp(0, newChromaRatio, 1))
+    },
+  ] as const
 }
 
 function createLightnessSignal() {
   const [Lightness, setLightness] = createSignal(0.6)
-  return [Lightness, (newLightness: number) => setLightness(clamp(0, newLightness, 1))] as const
+  return [
+    Lightness,
+    (newLightness: number) => {
+      if (Number.isNaN(newLightness)) return
+
+      setLightness(clamp(0, newLightness, 1))
+    },
+  ] as const
 }
